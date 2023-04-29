98c1548494c094ea78cd635213eae7cde46a9b7d
itamar hess <Itamardarihess@gmail.com>
2023-04-29 17:19:33 +0300

    better user expiriance

--- src/pages/ErrPage.jsx
+++ src/pages/ErrPage.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import style from "../styles/errPage.module.scss";
 import {useNavigate} from "react-router-dom"
-import {motion} from "framer-motion";
 
 const ErrPage = () => {
     const navigate = useNavigate();
+    const [isBtnClicked, setIsBtnClicked] = useState(false);
 
     function goToHomePage(){
             navigate("/");
     }
+
+    function btnClickAnimation(){
+        setIsBtnClicked(true);
+        setTimeout(() => {
+            setIsBtnClicked(false);
+        }, 300);
+    }
+
+    function btnClicked(){
+        btnClickAnimation();
+        setTimeout(() => {
+            goToHomePage();
+        }, 500);
+    }
+
     return ( 
         <div className={style.box}>
             <h1 className={style.title}>Page Not Found</h1>
-            <motion.button whileHover={{backgroundColor: "transparent"}} className={style.btn} onClick={goToHomePage}>go back</motion.button>
+            <button className={isBtnClicked === false ? style.btn : style["btn-active"]} onClick={btnClicked}>go back</button>
         </div>
      );
 }
