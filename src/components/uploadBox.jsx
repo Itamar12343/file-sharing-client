4f01c2e5f031141ffa7729f93570bc0266739ede
itamar hess <Itamardarihess@gmail.com>
2023-04-29 20:39:57 +0300

    new animations

--- src/components/uploadBox.jsx
+++ src/components/uploadBox.jsx
@@ -1,13 +1,21 @@
 import { useRef, useState } from "react";
 import style from "../styles/uploadBox.module.scss";
 import {CardImage} from "react-bootstrap-icons";
+import {motion} from "framer-motion";
 
 const UploadBox = () => {
     const [isBtnClicked, setIsBtnClicked] = useState(false);
+    const [isIconClicked, setIsIconClicked] = useState(false);
     const inputRef = useRef(null);
 
     function btnClick(){
         btnClickAnimation();
+        openFileExplorer();
+    }
+
+    function iconClick(){
+        iconClickAnimation();
+        openFileExplorer();
     }
 
     function btnClickAnimation(){
@@ -17,9 +25,20 @@ const UploadBox = () => {
         }, 300);
     }
 
+    function iconClickAnimation(){
+        setIsIconClicked(true);
+        setTimeout(() => {
+            setIsIconClicked(false);
+        }, 300);
+    }
+
+    function openFileExplorer(){
+        inputRef.current.click();
+    }
+
     return ( 
         <div className={style.box}>
-            <CardImage className={style.icon}/>
+            <CardImage className={ isIconClicked ? style["icon-active"] : style.icon} onClick={iconClick}/>
             <button className={isBtnClicked ? style["btn-active"] : style.btn} onClick={btnClick}>upload a file</button>
             <input type="file" ref={inputRef}/>
         </div>
