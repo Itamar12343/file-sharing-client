30ee0340983fb84546227b55d88557f3232a4261
my-name <Itamardarihess@gmail.com>
2023-10-27 13:42:26 +0300

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {Provider} from "react-redux";
 import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+     <Provider store={store}>
       <App />
       <ManageServer/>
     </Provider>
