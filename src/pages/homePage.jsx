3abbb131d40423bcc63e7d6c5403f526fda89ae3
my-name <Itamardarihess@gmail.com>
2023-10-30 18:22:08 +0200

    new animation

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,13 +2,14 @@ import style from "../styles/homePage.module.scss";
 import UploadBox from "../components/uploadBox";
 import Receive from "../components/receive";
 import { useState } from "react";
+import {motion} from "framer-motion";
 
 const HomePage = () => {
 
   const [shareAnimation, setShareAnimation] = useState(false);
   const [receiveAnimation, setReceiveAnimation] = useState(false);
 
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("normal");
 
 
   function clicked(btn){
@@ -40,8 +41,8 @@ const HomePage = () => {
     return ( 
       <>
         <div className={style.box}>
-          <button onClick={()=>{clicked("share")}} className={`${shareAnimation ? style["share-active"] : style.share} ${style.btn}`}><p className={style.p}>Share file</p></button>
-          <button onClick={()=>{clicked("receive")}} className={`${receiveAnimation ? style["receive-active"] : style.receive} ${style.btn}`}><p className={style.p}>Receive file</p></button>
+          {mode == "normal" && <button onClick={()=>{clicked("share")}} className={`${shareAnimation ? style["share-active"] : style.share} ${style.btn}`}><p className={style.p}>Share file</p></button>}
+          {mode == "normal" && <button onClick={()=>{clicked("receive")}} className={`${receiveAnimation ? style["receive-active"] : style.receive} ${style.btn}`}><p className={style.p}>Receive file</p></button>}
         </div>
 
         {mode == "share" && <UploadBox/>}
