fe504b7dae207d6f01c12a112b7761b0b6568047
my-name <Itamardarihess@gmail.com>
2023-11-09 00:20:12 +0200

    fix login bug

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,9 +5,24 @@ import {motion} from "framer-motion";
 const Login = () => {
 
     const [btnAnimation, setBtnAnimation] = useState(false);
+    const [name, setName] = useState();
+    const [closeLogin, setCloseLogin] = useState(false);
 
 
     function btnClicked(){
+       startBtnAnimation();
+       dealWithUserName();
+    }
+
+    function dealWithUserName(){
+        if(name != "" && name != undefined && name != " "){
+            localStorage.setItem("username", name);
+            console.log(name);
+            setCloseLogin(true);
+        }
+    }
+
+    function startBtnAnimation(){
         setBtnAnimation(true);
         setTimeout(() => {
             setBtnAnimation(false);
@@ -16,9 +31,9 @@ const Login = () => {
 
     return ( 
         <>
-        <motion.div initial={{transform: "scale(0) translate(-50%,-50%)"}} animate={{transform: "scale(1) translate(-50%,-50%)"}} className={style.box}>
+        <motion.div initial={{transform: "scale(0) translate(-50%,-50%)"}} animate={{transform: !closeLogin ? "scale(1) translate(-50%,-50%)" : "scale(0) translate(-50%,-50%)"}} className={style.box}>
             <h1 className={style.title}>Enter your name</h1>
-            <motion.input whileFocus={{border: "4px solid rgb(75, 159, 255)"}} className={style.input} type="text" placeholder="name"/>
+            <motion.input onChange={()=> setName(event.target.value)} whileFocus={{border: "4px solid rgb(75, 159, 255)"}} className={style.input} type="text" placeholder="name"/>
             <br/>
             <button className={btnAnimation ? style["btn-active"] : style.btn} onClick={btnClicked}>ok</button>
             <div className={style.block}></div>
