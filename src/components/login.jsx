d292c55bd69a9bfe4103053894174f4c39ce4daa
my-name <Itamardarihess@gmail.com>
2023-11-09 15:43:22 +0200

    final tweeks to login animations

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
 import style from "../styles/login.module.scss";
 import {motion} from "framer-motion";
+import {Check} from "react-bootstrap-icons";
 
 const Login = () => {
 
     const [btnAnimation, setBtnAnimation] = useState(false);
+    const [checkAnimation, setCheckAnimation] = useState(false);
     const [name, setName] = useState();
     const [closeLogin, setCloseLogin] = useState(false);
 
 
     function btnClicked(){
-       startBtnAnimation();
        dealWithUserName();
     }
 
     function dealWithUserName(){
         if(name != "" && name != undefined && name != " "){
             localStorage.setItem("username", name);
-            console.log(name);
-            setCloseLogin(true);
+            startBtnAnimation();
         }
     }
 
     function startBtnAnimation(){
+        setCheckAnimation(true);
         setBtnAnimation(true);
         setTimeout(() => {
             setBtnAnimation(false);
+            setTimeout(() => {
+                setCloseLogin(true);
+            }, 1000);
         }, 300);
     }
 
@@ -35,7 +39,7 @@ const Login = () => {
             <h1 className={style.title}>Enter your name</h1>
             <motion.input onChange={()=> setName(event.target.value)} whileFocus={{border: "4px solid rgb(75, 159, 255)"}} className={style.input} type="text" placeholder="name"/>
             <br/>
-            <button className={btnAnimation ? style["btn-active"] : style.btn} onClick={btnClicked}>ok</button>
+            <motion.button style={{backgroundColor: checkAnimation ? "rgb(54, 160, 253)" : "transparent"}} className={btnAnimation ? style["btn-active"] : style.btn} onClick={btnClicked}>{checkAnimation ? <div className={style["icon-container"]}><Check className={style.check}/></div> : "ok"}</motion.button>
             <div className={style.block}></div>
             </motion.div>
         </>
